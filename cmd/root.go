1f25179660730371283cd3a97c74cbe7f747bf9b
Rudi Farkas <rudi.farkas@gmail.com>
2024-02-10 18:31:01 +0100

    update README

--- cmd/root.go
+++ cmd/root.go
@@ -21,21 +21,25 @@ var rootCmd = &cobra.Command{
 
 	This evolving set of tools requires a compatible set of patches to the CUE source code (*).
 
-	As of 7 Jan 2024, the CUE patch extras are turned on by setting these environment variables to non-empty values:
+	CUE patch extras are turned on by setting these environment variables to non-empty values:
 
-	CUEDO_AST_SPEW - turns on the AST spew mode
+	CUEDO_AST_SPEW - turns on the AST spew mode (deep structure dump)
 	CUEDO_AST_TREE - turns on the AST tree mode
 	CUEDO_AST_TYPE - turns on the AST type mode
 	CUEDO_FBB_KLUDGE - turns on a kludge in formatter to anticipate a fix for the issue #2567
 	CUEDO_FORMATTER_HEXDUMP - turns on hex dump of the formatter's internal buffer, as each fragment is printed
 	CUEDO_FORMATTER_STACKTRACE - turns on dump of a stack trace in the formatter, as each fragment is printed
+	CUEDO_PARSER_COMMENTS_STACK - turns on the print of the commentState stack
 	CUEDO_PARSER_COMMENTS_POS - adds to the parser.Trace mode data about the comment positions and texts
 	CUEDO_PARSER_DEBUG_STR - turns on the parser debug string mode
+	CUEDO_PARSER_STACKTRACE - turns on the parser call stack trace mode
+	CUEDO_PARSER_TOKEN - turns on print of the parser tokens
 	CUEDO_PARSER_TRACE - turns on the parser.Trace mode
+	CUEDO_PROCESS_BOTH_WITH_AND_WITHOUT_COMMENTS - turns on the parsing both with and without comments
 
 	Note that these patches are not part of the official CUE source code and are not supported by the CUE team.
 
-	(*) branch https://github.com/rudifa/cue/tree/change-1173870-cuedo
+	(*) branch https://github.com/rudifa/cue/tree/master
 	`,
 	// Run: func(cmd *cobra.Command, args []string) {
 	// },
